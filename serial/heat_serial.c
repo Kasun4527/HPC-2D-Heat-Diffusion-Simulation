9b09e38fb6cb0dffeb10598f5be9f5f30971f1d5
Yasiith <yasithvimukthi259@gmail.com>
2026-03-08 23:20:02 +0530

    Add command-line parameter parsing for simulation configuration

--- serial/heat_serial.c
+++ serial/heat_serial.c
@@ -13,6 +13,21 @@
 
 
 int main(int argc, char *argv[]) {
+    // Default parameters
+    int rows = 1000;
+    int cols = 1000;
+    int timesteps = 1000;
+    double alpha = 0.1;
+    
+    // Parse command line arguments
+    if (argc >= 2) rows = atoi(argv[1]);
+    if (argc >= 3) cols = atoi(argv[2]);
+    if (argc >= 4) timesteps = atoi(argv[3]);
+    if (argc >= 5) alpha = atof(argv[4]);
+    
     printf("Serial Heat Diffusion Simulation\n");
+    printf("Grid size: %d x %d\n", rows, cols);
+    printf("Timesteps: %d\n", timesteps);
+    printf("Alpha: %f\n\n", alpha);
     return 0;
 }
