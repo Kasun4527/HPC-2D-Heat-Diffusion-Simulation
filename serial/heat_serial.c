2a026ef64daa1116157423ee8a21de29f787e9e1
Yasiith <yasithvimukthi259@gmail.com>
2026-03-08 23:18:53 +0530

    Add documentation header and common includes

--- serial/heat_serial.c
+++ serial/heat_serial.c
@@ -1,5 +1,16 @@
+/*
+ * Serial Heat Diffusion Simulation
+ *
+ * Baseline serial implementation of a 2D heat diffusion model.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "../common/grid.h"
+#include "../common/utils.h"
+
 
 int main(int argc, char *argv[]) {
     printf("Serial Heat Diffusion Simulation\n");
