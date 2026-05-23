4309b836f1ead72f51845d5df32af2510132286d
kasungunasekara <162046889+Kasun4527@users.noreply.github.com>
2026-05-23 20:22:38 +0530

    mip initialisation changes

--- mpi/heat_mpi.c
+++ mpi/heat_mpi.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); //Giving idis to all process
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &size); //size of the process(number of process)
 
     int rows = 1000;
     int cols = 1000;
